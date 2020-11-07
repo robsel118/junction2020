5d896a5034d5aa764194d94b80913503e930a586
Matti Mäki-Kihniä <matti.makikihnigmail.com>
2020-11-07 10:29:00 +0200

    Initial logic

---
+++ Scale/data_service.py
@@ -0,0 +1,9 @@
+from firebase import firebase
+import sys
+sys.path.insert(1, '/hx711/hx711py')
+import example
+
+firebase = firebase.FirebaseApplication('https://junction2020-1d1be.firebaseio.com/', None)
+#result = firebase.post('https://junction2020-1d1be.firebaseio.com/', {'cTemp':str(1.23),'ftemp':str(2.23), 'humidity':str(3.4)})
+result = firebase.get('/scales', None)
+print(result)
