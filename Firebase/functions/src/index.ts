3b31f2634d30ad35b1b52571f20289e19354689e
Matti Mäki-Kihniä <matti.makikihnigmail.com>
2020-11-07 16:19:20 +0200

    Added logic to handle incoming requests to /addMeasurement

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -1,9 +1,24 @@
-import * as functions from 'firebase-functions';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+import { Response } from 'express'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+// handle incoming reqs to 
+exports.addMeasurement = functions.https.onRequest(async (req: { body:Record<string, any> }, res: Response) => {
+    const itemWeight = req.body["weight"]
+    const scale = req.body["scale"]
+    functions.logger.log('We gone some value ' + itemWeight + ' with scale ' + scale);
+
+    admin.database().ref('/measurements/' + scale + '/inputs').push({
+        weight: itemWeight,
+      }).then(() => {
+        console.log('New Message written');
+        // Returning the sanitized message to the client.
+        res.json({ text: "message added" });
+      })
+        .catch((error: any) => {
+            // Re-throwing the error as an HttpsError so that the client gets the error details.
+            throw new functions.https.HttpsError('unknown', error.message, error);
+        });
+  });
