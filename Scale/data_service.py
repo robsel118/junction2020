c9f4011cafe4eb6fb8f0dfc768b10200d7bb8f72
Matti Mäki-Kihniä <matti.makikihnigmail.com>
2020-11-07 12:39:14 +0200

    Added data_service.py

---
+++ Scale/data_service.py
@@ -0,0 +1,42 @@
+from firebase import firebase
+import sys
+sys.path.insert(1, 'hx711/hx711py')
+import example
+
+firebase_db_url = 'INSERT URL'
+firebase = firebase.FirebaseApplication(firebase_db_url, None)
+
+previousValue = 0.0
+VAL_THRESHOLD = 0.4 # TODO : this should be changed based on the item used
+MIN_WEIGHT_THRESHOLD = 150.0 # TODO : this should be changed based on the item used
+
+def scaleCallback(newValue):
+	global previousValue
+
+	# setting of the inital value
+	if previousValue == 0.0 and newValue > MIN_WEIGHT_THRESHOLD:
+		previousValue = newValue
+		print('Set new value ' + str(previousValue))
+	# handle case when user presses on the item on the scale (pump bottle)
+	elif (newValue - previousValue) > previousValue * 2 and (newValue - previousValue) >= 150:
+		print('User most likely consumed by pressing on the scale skipping')
+		return
+	# should hit when a single usage is made
+	elif (previousValue - newValue) >= VAL_THRESHOLD:
+		diff = previousValue - newValue
+		previousValue = newValue
+		print('Value changed to ' + str(previousValue) + ' with diff ' + str(diff))
+	else:
+		print('Previous value is ' + str(previousValue) + ' new value is ' + str(newValue) + ' diff is too small, skipping')
+
+def main():
+	result = firebase.get('/scales', None)
+	print(result)
+	example.poll(scaleCallback)
+
+if __name__ == "__main__":
+    main()
+
+
+
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch Scale/data_service.py' --prune-empty --tag-name-filter cat -- --all
