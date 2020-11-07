45517f72ebcdf5bf84037c24582d1038b3ab729b
Matti Mäki-Kihniä <matti.makikihnigmail.com>
2020-11-07 12:03:54 +0200

    Once again logic

--- Scale/data_service.py
+++ Scale/data_service.py
@@ -6,22 +6,27 @@ firebase = firebase.FirebaseApplication('https://junction2020-1d1be.firebaseio.c
 
 previousValue = 0.0
 VAL_THRESHOLD = 0.4 # TODO : this should be changed based on the item used
-MIN_WEIGHT_THRESHOLD = 50.0
+MIN_WEIGHT_THRESHOLD = 150.0
+
 
 
 def scaleCallback(newValue):
 	global previousValue
+
+
+	# setting of the inital value
 	if previousValue == 0.0 and newValue > MIN_WEIGHT_THRESHOLD:
 		previousValue = newValue
 		print('Set new value ' + str(previousValue))
+	# handle case when user presses on the item on the scale (pump bottle)
+	elif (newValue - previousValue) > previousValue * 2 and (newValue - previousValue) >= 150:
+		print('User most likely consumed by pressing on the scale skipping')
+		return
+	# should hit when a single usage is made
 	elif (previousValue - newValue) >= VAL_THRESHOLD:
 		diff = previousValue - newValue
 		previousValue = newValue
 		print('Value changed to ' + str(previousValue) + ' with diff ' + str(diff))
-		# set value to firebase!
-	elif (newValue - previousValue) <= 200.0 and (newValue - previousValue) > 50:
-		previousValue = newValue
-		print('New value set! ' + str(previousValue))
 	else:
 		print('Previous value is ' + str(previousValue) + ' new value is ' + str(newValue) + ' diff is too small, skipping')
 
